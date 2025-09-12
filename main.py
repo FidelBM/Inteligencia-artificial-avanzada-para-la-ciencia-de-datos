441c6adf68a9c5bcdf84451a9b13b1b9b11dc22d
FidelBM <a01798199@tec.mx>
2025-09-11 23:30:10 -0600

    correcciones

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-from regression_log import LogisticRegressionFidel
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
